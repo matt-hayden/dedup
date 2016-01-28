6ef3abdf5355319db5ed31cb421a3e54a30432d6
Matt Hayden <hayden+github@morp.holo.gy>
2016-01-28 12:55:54 -0700

    Rewrote dedup

--- db.py
+++ db.py
@@ -2,10 +2,12 @@
 
 """
 """
+import collections
 import os, os.path
 import shelve
 import re
 
+from __init__ import *
 from futil import *
 
 
@@ -42,12 +44,15 @@ class Database:
 					return False
 		self.db[k] = get_file_info(fullpath)
 		return True
-	def add_directory(self, arg, callback=None):
+	def add_directory(self, arg, callback=None, ignore_dotfiles=True, ignore_symlinks=True):
 		for root, dirs, files in os.walk(arg, topdown=True):
-			files = [ f for f in files if not f.startswith('.') ]
-			dirs = [ d for d in dirs if not d.startswith('.') ]
+			if ignore_dotfiles:
+				files = [ f for f in files if not f.startswith('.') ]
+				dirs = [ d for d in dirs if not d.startswith('.') ]
 			for f in files:
 				fullpath = os.path.join(root, f)
+				if ignore_symlinks and os.path.islink(fullpath):
+					continue
 				if self.add_entry(fullpath) and callback:
 					callback(fullpath)
 	def del_entry(self, arg):
@@ -65,8 +70,47 @@ class Database:
 				continue
 			fullpath = os.path.join(self.root, k)
 			self.add_entry(full_path)
-
+	def get_sums_frequencies(self, recurse_archives=True):
+		"""Returns (duplicate, unique) characteristics found in the database
+		"""
+		freqs = collections.Counter()
+		for vs in self.db.values():
+			freqs.update(vs.sums)
+			if recurse_archives and hasattr(vs, 'members'):
+				for i in vs.members.values():
+					freqs.update(i.sums)
+		mc = freqs.most_common()
+		for n, (i, c) in enumerate(mc):
+			if c == 1:
+				break
+		return mc[:n], [ i for i, c in mc[n:] ]
+	def get_possible_duplicates(self, MIN_WEIGHT=4):
+		"""Does not recurse into archives
+		"""
+		nunique_freqs, _ = self.get_sums_frequencies()
+		search_chars = set()
+		for c, _ in nunique_freqs:
+			w = MATCH_WEIGHTS.get(c[0])
+			if w:
+				if MIN_WEIGHT <= w:
+					search_chars.update([c])
+			else:
+				print('Interestingly, {} is found but not weighted'.format(c))
+		for f, i in self.db.items():
+			if i.sums & search_chars:
+				yield f, i
 	def dedup(self):
+		"""Does not recurse into archives
+		"""
+		dp = dict(self.get_possible_duplicates())
+		while len(dp):
+			t_f, t_i = dp.popitem()
+			dups = [ (f, i) for (f, i) in dp.items() if t_i == i ]
+			for f, i in dups:
+				yield f, i
+				del dp[f]
+				self.del_entry(f)
+	def dedup2(self):
 		"""Modified argument in-place, generating tuple of duplicates
 		"""
 		def is_valid(filename):
